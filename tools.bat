a267e68f4abc213a81fa39946f5acb03f3f88b83
Crovax <76233401+CrovaxTheCursed@users.noreply.github.com>
2022-01-21 09:59:08 -0700

    Add files via upload

---
+++ tools.bat
@@ -0,0 +1,33 @@
+@echo off
+
+choco install hollowshunter -y
+choco install pesieve -y
+choco install pebear -y
+choco install processhacker -y
+choco install sysinternals -y
+choco install x64dbg.portable -y
+choco install dnspy -y
+choco install fiddler -y
+choco install explorersuite -y
+choco install reshack -y
+choco install hxd -y
+choco install baretail -y
+choco install notepadplusplus -y
+choco install yasm -y
+choco install git -y
+choco install cmake -y
+choco install wireshark -y 
+choco install ppee -y
+choco install pestudio -y
+choco install ghidra -y
+choco install capa -y
+choco install 010editor -y
+choco install vscode -y
+choco install vscode-jupyter -y
+choco install die -y
+choco install yara -y
+choco install windowsxappremover -y 
+
+echo Finished!
+pause
+
